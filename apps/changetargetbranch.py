d603a5887b8e1c63b80a85a64b64476133b0168c
zhujian <jiazhu@redhat.com>
2024-12-02 11:33:22 +0800

    Support change branch for acm repos
    Signed-off-by: zhujian <jiazhu@redhat.com>

--- apps/changetargetbranch.py
+++ apps/changetargetbranch.py
@@ -73,16 +73,39 @@ def update_pr_title(repo, pr_number, title):
             print(f"Error updating title of PR #{pr_number} in {repo}: {e}")
 
 
+# Function to get a list of changed files in a PR
+def get_changed_files(repo, pr_number):
+    try:
+        # Using gh command to get the list of changed files in the PR
+        result = subprocess.run(
+            ['gh', 'pr', 'view', str(pr_number), '--repo', repo, '--json', 'files'],
+            capture_output=True, text=True, check=True
+        )
+        pr_data = json.loads(result.stdout)
+        files = [file['path'] for file in pr_data['files']]
+        return files
+    except subprocess.CalledProcessError as e:
+        print(f"Error fetching changed files for PR #{pr_number} in {repo}: {e}")
+        return []
+
+# Function to check if all changed files are in the ".tekton" directory
+def all_files_in_tekton(files):
+    for file in files:
+        if not file.startswith('.tekton/'):
+            return False
+    return True
+
+
 @click.command()
 @click.argument("repos", nargs=-1)
 @click.option("--approve", is_flag=True, default=False, help="set to true will comment /lgtm on the PR")
-@click.option("--branch-before-changing", type=click.STRING, default="backplane-2.8", help="the target branch name of the PR before changing")
-@click.option("--branch-after-changing", type=click.STRING, default="main", help="the target branch name of the PR after changing")
 @click.option("--extra-keyword", type=click.STRING, help="the extra keyword to search for in PR titles")
 # Main function to list and modify PRs
 # Example usage: python -m apps.changetargetbranch --approve --extra-keyword="Konflux ..." stolostron/ocm stolostron/managed-serviceaccount
-def main(repos, approve, branch_before_changing, branch_after_changing, extra_keyword):
-    print(f"Repos: {repos}, Approve: {approve}, Branch Before: {branch_before_changing}, Branch After: {branch_after_changing}")
+def main(repos, approve, extra_keyword):
+    branches_before_changing = ["backplane-2.8", "release-2.13"]
+    branch_after_changing = "main"
+    print(f"Repos: {repos}, Approve: {approve}, Branches Before: {branches_before_changing}, Branch After: {branch_after_changing}")
     keywords = ['Update Konflux references', 'Red Hat Konflux', 'chore(deps): update konflux references']
     if extra_keyword:
         keywords.append(extra_keyword)
@@ -90,24 +113,35 @@ def main(repos, approve, branch_before_changing, branch_after_changing, extra_ke
     # if repos is not provided, use the default list
     if not repos:
         repos = [
-            # "stolostron/ocm", # zhujian7
+            "stolostron/ocm", # zhujian7
             "stolostron/managed-serviceaccount", # zhujian7
             "stolostron/multicloud-operators-foundation", # elgnay
+            "stolostron/managedcluster-import-controller", # xuezhaojun
+            "stolostron/clusterlifecycle-state-metrics", # haoqing0110
+            "stolostron/klusterlet-addon-controller", # zhiweiyin318
         ]
     for repo in repos:
         prs = list_prs(repo)
         for pr in prs:
+            title_match = False
+            content_match = False
+            # Check if all files changed are in the ".tekton" directory, and comment "/lgtm" if true
+            changed_files = get_changed_files(repo, pr['number'])
+            if all_files_in_tekton(changed_files):
+                content_match = True
             if any(keyword in pr['title'] for keyword in keywords):
-                # print(f"PR #{pr['number']} in {repo} targets {pr['title']}")
-
+                title_match = True
+            print(f"PR #{pr['number']} in {repo}, content_match: {content_match}, title_match: {title_match}, title: {pr['title']}")
+            if title_match and content_match:
                 # Check if the PR title starts with ":seeding:" and update it if not
                 update_pr_title(repo, pr['number'], pr['title'])
 
-                if pr['baseRefName'] == branch_before_changing:
-                    if branch_before_changing == branch_after_changing:
-                        print(f"PR #{pr['number']} in {repo} already targets {branch_before_changing}. Skipping.")
+                # if pr['baseRefName'] == branch_before_changing:
+                if pr['baseRefName'] in branches_before_changing:
+                    if pr['baseRefName'] == branch_after_changing:
+                        print(f"PR #{pr['number']} in {repo} already targets {branch_after_changing}. Skipping.")
                     else:
-                        print(f"PR #{pr['number']} in {repo} targets {branch_before_changing}. Changing to {branch_after_changing}.")
+                        print(f"PR #{pr['number']} in {repo} targets {pr['baseRefName']}. Changing to {branch_after_changing}.")
                         change_base_branch(repo, pr['number'], branch_after_changing)
 
                 if approve:
